9264e846806f532d617afabafe61dec1d84ca184
huynhthuan <huynhthuanvugia@gmail.com>
2021-01-10 15:55:44 +0700

    Done

--- js/script.js
+++ js/script.js
@@ -4,4 +4,45 @@ $(document).ready(function () {
         arrows: false,
         slidesToShow: 3,
     });
+
+    $('.balance-slide').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+    });
+
+    $('.filter-btn').click(function () {
+        let filterUp = $(this).find('.market-filter .filter-up');
+        let filterDown = $(this).find('.market-filter .filter-down');
+        if (filterUp.hasClass('active')) {
+            filterUp.removeClass('active');
+            filterDown.toggleClass('active');
+        } else {
+            filterDown.removeClass('active');
+            filterUp.toggleClass('active');
+        }
+    });
+
+    $('.trade-progress-bar').each(function (index, item) {
+        let self = $(item);
+        let bgColor = $(item).data('color');
+        let bgPercent = $(item).data('percent');
+
+        self.css("background-color", bgColor);
+        self.css("width", bgPercent + '%');
+    });
+
+    $('#trade-main-slide').slider({
+        max: 100,
+        min: 25,
+        step: 25
+    });
+
+    let tradeSlideHandle = $('.trade-panel-slider .ui-slider-handle');
+
+    $( "#trade-main-slide" ).on( "slidechange", function( event, ui ) {
+        if(ui.value === 100){
+            tradeSlideHandle.css('left', 'calc(100% - 10px)')
+        }
+    } );
 });
